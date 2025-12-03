2a68aabbe37405d34792b5ee604fa25be9ef3db9
Luis Angel Baltazar Gonzalez <luisangelbaltazarglz@outlook.com>
2025-12-02 23:47:30 -0600

    Conexion basica de base de datos

---
+++ back/index.js
@@ -0,0 +1,24 @@
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const mysql = require('mysql2');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+//Aqui conectaremos la BD
+const db = mysql.createConnection(process.env.DATABASE_URL || '');
+
+app.get('/api/test', (req, res) => {
+    res.json({
+        mensaje: "¡Backend conectado exitosamente!",
+        fecha: new Date().toISOString()
+    });
+});
+
+const PORT = process.env.PORT || 4000;
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en puerto ${PORT}`);
+});
