5e3db581989277e1f3ac1b9c11b64d4cd9b9a7f4
Luis Angel Baltazar Gonzalez <luisangelbaltazarglz@outlook.com>
2025-12-03 04:19:53 -0600

    corregido codigo

--- back/server.js
+++ back/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const mysql = require('mysql2'); 
-const bcrypt = require('bcryptjs');
+
 const authRoutes = require('./routes/auth.routes'); 
 
 const app = express();
